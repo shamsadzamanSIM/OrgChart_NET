f3c41cb1951c639a7fb05f3ef74d8fe08ec430f3
nberent <nberent-spillson@systemsinmotion.com>
2014-05-20 09:47:38 -0400

    Added logins

---
+++ Orgchart.Database/Security/OrgchartUser_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [OrgchartUser]
+    WITH PASSWORD = N'!<tm{irNdpQnfja,ym2j{fiqmsFT7_&#$!~<nybE!ny}noct', SID = 0x0F8F478190F5404EA11088EA56FBF22B, DEFAULT_DATABASE = [Orgchart], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
+
--- Orgchart.Database/Security/RoleMemberships.sql
+++ Orgchart.Database/Security/RoleMemberships.sql
@@ -8,3 +8,7 @@ ALTER ROLE [db_datawriter] ADD MEMBER [OrgchartUser];
 GO
 ALTER ROLE [db_datareader] ADD MEMBER [OrgchartUser];
 
+
+GO
+
+
