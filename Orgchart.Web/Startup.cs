845ef228dfc2b1fc25ff89ed9be34edcab30f46c
nberent <nberent-spillson@systemsinmotion.com>
2014-05-20 08:44:32 -0400

    Core Project

---
+++ Orgchart.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Orgchart.Web.Startup))]
+namespace Orgchart.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
