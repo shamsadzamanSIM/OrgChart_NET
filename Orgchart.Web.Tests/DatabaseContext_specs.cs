c4fe0fddcb2d2f6e0ff998e407fe4cc90c9418cc
shamsadzaman <szaman@systemsinmotion.com>
2014-05-21 11:00:50 -0400

    OCN-14 Created connection string and integration test

---
+++ Orgchart.Web.Tests/DatabaseContext_specs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orgchart.Web.Tests
+{
+    [TestClass]
+    public class DatabaseContext_specs
+    {
+        [TestMethod]
+        public void Can_connecto_to_Database()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DatabaseContext"].ToString();
+            var conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+                Assert.IsTrue(conn.State == ConnectionState.Open);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Assert.Fail(e.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+        }
+    }
+}
